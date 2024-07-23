72f932bc632d445b6f654235f02505e979bf6e70
Ricky-Patel <ricky.patel.git@gmail.com>
2024-07-23 17:33:10 -0400

    adding existing files to github

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import TodoTable from "./TodoTable";
+
+function App() {
+  return (
+    <div>
+      <h1> Todo - it! </h1>
+      <span>The things you need to do are below, just do it!</span>
+      <TodoTable />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TodoTable.jsx
@@ -0,0 +1,9 @@
+//import React from "react";
+
+const TodoTable = () => {
+return (
+<div>a table of the todo items</div>
+)
+}
+
+export default TodoTable;
